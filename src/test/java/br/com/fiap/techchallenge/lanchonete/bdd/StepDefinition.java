ed2a5e65b0d0af804a6af39722110b43bab3e253
Pedro Pereira <pedrojr.19@hotmail.com>
2024-01-06 16:38:41 -0300

    feat: adiciona estrutura inicial para testes bdd

---
+++ src/test/java/br/com/fiap/techchallenge/lanchonete/bdd/StepDefinition.java
@@ -0,0 +1,105 @@
+package br.com.fiap.techchallenge.lanchonete.bdd;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+class StepDefinition {
+
+    @Quando("preencher todos os dados para cadastro do produto")
+    public void preencherTodosDadosParaCadastrarProduto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("o produto deve ser criado com sucesso")
+    public void produtoDeveSerCriadoComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("deve exibir o produto cadastrado")
+    public void deveExibirProdutoCadastrado() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dado("que um produto já está cadastrado")
+    public void produtoJaCadastrado() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("realizar a busca do produto")
+    public void realizarBuscaProduto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("o produto deve ser exibido com sucesso")
+    public void produtoDeveSerExibidoComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("realizar a requisição para alterar o produto")
+    public void realizarRequisicaoParaAlterarProduto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("o produto deve ser alterado com sucesso")
+    public void produtoDeveSerAlteradoComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("deve exibir o produto alterado")
+    public void deveExibirProdutoAlterado() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("realizar a requisição para remover o produto")
+    public void realizarRequisicaoParaRemoverProduto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("o produto deve ser removido com sucesso")
+    public void produtoDeveSerRemovidoComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("requisitar a lista de todos os produtos")
+    public void requisitarListaProdutos() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("os produtos devem ser exibidos com sucesso")
+    public void produtosDevemSerExibidosComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("realizar a busca do produto por categoria")
+    public void realizarBuscaProdutoPorCategoria() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("realizar a requisição para alterar a imagem do produto")
+    public void realizarRequisicaoParaAlterarImagemProduto() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Então("a imagem do produto deve ser alterada com sucesso")
+    public void imagemProdutoDeveSerAlteradaComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
